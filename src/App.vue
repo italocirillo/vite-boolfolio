0a6e328d522df1751e97adc74558ebb40f317510
italocirillo <italocirillo@gmail.com>
2023-06-14 00:24:11 +0200

    ESERCIZIO COMPLETO

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
-import axios from 'axios'
+import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
 
 export default {
   data() {
@@ -16,14 +17,34 @@ export default {
         this.projects = resp.data.results;
       })
     }
+  },
+  components: {
+    ProjectCard,
   }
 }
 </script>
 
 <template>
-  <h1>Hello world</h1>
+  <div class="container">
+    <h1>PROJECT</h1>
+    <div class="row row-cols-3 g-3">
+      <div class="col" v-for="project in projects" :key="project.id">
+        <ProjectCard :project="project" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
 @use "./styles/general.scss";
+
+body {
+  background-color: rgb(0, 93, 93);
+}
+
+h1 {
+  color: white;
+  padding: 2em;
+  text-align: center;
+}
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: Object
+    },
+    data() {
+        return {
+            baseUrl: "http://127.0.0.1:8000",
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card h-100">
+        <img v-if="project.image"
+            :src="project.image.startsWith(`https://via.placeholder.com/`) ? `${project.image}` : `${baseUrl}/storage/${project.image}`"
+            class="card-img-top" :alt="project.title" />
+        <div v-else>
+            Image not found
+        </div>
+        <div class="card-body">
+            <h5>{{ project.title }}</h5>
+            <p>{{ project.description }}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
