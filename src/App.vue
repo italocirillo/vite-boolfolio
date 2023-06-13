3b806be5888918cceae6f0c2381fc802b8c27764
italocirillo <italocirillo@gmail.com>
2023-06-13 23:35:50 +0200

    CHIAMATA AXIOS ESEGUITA CORRETTAMENTE

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <script>
-export default {
+import axios from 'axios'
 
+export default {
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects').then(resp => {
+        this.projects = resp.data.results;
+      })
+    }
+  }
 }
 </script>
 
